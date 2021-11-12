dde6b49850dabf8db7e9c7e401ad60b67aed3aaa
Ryan Tucker <nick.tucker.bass@gmail.com>
2021-11-11 20:36:16 -0500

    deployment

--- src/components/_helpers/environment.ts
+++ src/components/_helpers/environment.ts
@@ -4,8 +4,8 @@ switch (window.location.hostname) {
   case "localhost" || "127.0.0.1":
     API_URL = "http://localhost:3333";
     break;
-  case "tmor-pet-tinder-client.herokuapp.com":
-    API_URL = "https://first-call.herokuapp.com";
+  case "firstcallapp.herokuapp.com":
+    API_URL = "https://api-firstcall.herokuapp.com/";
 }
 
 export default API_URL;
